b4fac09025cb812d813ab9081f45ec1ce55c571d
takeshi <takeshi@192.168.1.6>
2017-09-30 15:31:11 +0900

    First Commit

---
+++ src/main/Hello.java
@@ -0,0 +1,16 @@
+package main;
+
+public class Hello {
+	
+	private String Name;
+	private int Hp;
+	
+	void setName(String inName) {
+		this.Name = inName;
+	}
+	
+	void showName() {
+		System.out.println("Name:" + this.Name );
+	}
+
+}
---
+++ src/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("test");
+		
+		Hello hello1 = new Hello();
+		
+		hello1.setName("Takeshi2");
+		hello1.showName();
+		
+	}
+
+}
